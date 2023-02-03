f096c8c5152c2a801abffc4f7226bd30dd9114e5
Himank Barve <hbarve1592@gmail.com>
2023-02-03 15:34:16 +0530

    simple particle system created

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ const canvas = document.createElement("canvas");
 
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
+const particlesArray: Particle[] = [];
 
 canvas.width = windowWidth;
 canvas.height = windowHeight;
@@ -68,10 +69,55 @@ function drawCircle() {
   ctx.fill();
 }
 
+class Particle {
+  x: number | undefined;
+  y: number | undefined;
+  size: number;
+  speedX: number;
+  speedY: number;
+
+  constructor() {
+    // this.x = mouse.x;
+    // this.y = mouse.y;
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+
+  update() {
+    this.x = this.x! + this.speedX;
+    this.y = this.y! + this.speedY;
+  }
+
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.arc(this.x!, this.y!, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function init() {
+  for (let i = 0; i < 100; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+init();
+
+function handleParticles() {
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+  }
+}
+
 // drawCircle();
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
   requestAnimationFrame(animate);
-  drawCircle();
+  // drawCircle();
 }
 animate();
